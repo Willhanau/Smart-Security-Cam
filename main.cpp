d1aa0f4ece8472dd2a205f8cd951a558ff58f321
Will <willhanau@gmail.com>
2018-12-05 19:57:03 -0800

    API request to Google Vision works! Updated mbed_app_example.json to be easier to understand.

--- main.cpp
+++ main.cpp
@@ -25,6 +25,28 @@ const char SSL_CA_PEM[] = "-----BEGIN CERTIFICATE-----\n"
                         "PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls\n"
                         "YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk\n"
                         "CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=\n"
+                        "-----END CERTIFICATE-----\n"
+                        "-----BEGIN CERTIFICATE-----\n"
+                        "MIIDujCCAqKgAwIBAgILBAAAAAABD4Ym5g0wDQYJKoZIhvcNAQEFBQAwTDEgMB4G\n"
+                        "A1UECxMXR2xvYmFsU2lnbiBSb290IENBIC0gUjIxEzARBgNVBAoTCkdsb2JhbFNp\n"
+                        "Z24xEzARBgNVBAMTCkdsb2JhbFNpZ24wHhcNMDYxMjE1MDgwMDAwWhcNMjExMjE1\n"
+                        "MDgwMDAwWjBMMSAwHgYDVQQLExdHbG9iYWxTaWduIFJvb3QgQ0EgLSBSMjETMBEG\n"
+                        "A1UEChMKR2xvYmFsU2lnbjETMBEGA1UEAxMKR2xvYmFsU2lnbjCCASIwDQYJKoZI\n"
+                        "hvcNAQEBBQADggEPADCCAQoCggEBAKbPJA6+Lm8omUVCxKs+IVSbC9N/hHD6ErPL\n"
+                        "v4dfxn+G07IwXNb9rfF73OX4YJYJkhD10FPe+3t+c4isUoh7SqbKSaZeqKeMWhG8\n"
+                        "eoLrvozps6yWJQeXSpkqBy+0Hne/ig+1AnwblrjFuTosvNYSuetZfeLQBoZfXklq\n"
+                        "tTleiDTsvHgMCJiEbKjNS7SgfQx5TfC4LcshytVsW33hoCmEofnTlEnLJGKRILzd\n"
+                        "C9XZzPnqJworc5HGnRusyMvo4KD0L5CLTfuwNhv2GXqF4G3yYROIXJ/gkwpRl4pa\n"
+                        "zq+r1feqCapgvdzZX99yqWATXgAByUr6P6TqBwMhAo6CygPCm48CAwEAAaOBnDCB\n"
+                        "mTAOBgNVHQ8BAf8EBAMCAQYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUm+IH\n"
+                        "V2ccHsBqBt5ZtJot39wZhi4wNgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2NybC5n\n"
+                        "bG9iYWxzaWduLm5ldC9yb290LXIyLmNybDAfBgNVHSMEGDAWgBSb4gdXZxwewGoG\n"
+                        "3lm0mi3f3BmGLjANBgkqhkiG9w0BAQUFAAOCAQEAmYFThxxol4aR7OBKuEQLq4Gs\n"
+                        "J0/WwbgcQ3izDJr86iw8bmEbTUsp9Z8FHSbBuOmDAGJFtqkIk7mpM0sYmsL4h4hO\n"
+                        "291xNBrBVNpGP+DTKqttVCL1OmLNIG+6KYnX3ZHu01yiPqFbQfXf5WRDLenVOavS\n"
+                        "ot+3i9DAgBkcRcAtjOj4LaR0VknFBbVPFd5uRHg5h6h+u/N5GJG79G+dwfCMNYxd\n"
+                        "AfvDbbnvRG15RjF+Cv6pgsH/76tuIMRQyV+dTZsXjAzlAcmgQWpzU/qlULRuJQ/7\n"
+                        "TBj0/VLZjmmx6BEP3ojY+x1J96relc8geMJgEtslQIxq/H5COEBkEveegeGTLg==\n"
                         "-----END CERTIFICATE-----\n";
 
 void Blink_LED(){
@@ -60,8 +82,27 @@ void Twilio_https_Request(NetworkInterface *network){
     HttpsRequest* request = new HttpsRequest(network, SSL_CA_PEM, HTTP_POST, MBED_CONF_APP_TWILIO_API_URL);
     request->set_header("Content-Type", "application/x-www-form-urlencoded");
     request->set_header("Authorization", MBED_CONF_APP_TWILIO_BASIC_AUTH);
-    //const char body[] = "To=%2B16199475312&From=%2B18582953031&MediaUrl=https%3A%2F%2Fdemo.twilio.com%2Fowl.png&Body=Hello+From+Nucleo+Board!";
     HttpResponse* response = request->send(MBED_CONF_APP_TWILIO_REQUEST_BODY, strlen(MBED_CONF_APP_TWILIO_REQUEST_BODY));
+    
+    //Print Http Response
+    printf("Status: %d - %s\n", response->get_status_code(), response->get_status_message().c_str());
+    printf("Headers:\n");
+    for (size_t ix = 0; ix < response->get_headers_length(); ix++) {
+        printf("\t%s: %s\n", response->get_headers_fields()[ix]->c_str(), response->get_headers_values()[ix]->c_str());
+    }
+    printf("\nBody (%lu bytes):\n\n%s\n", response->get_body_length(), response->get_body_as_string().c_str());
+    
+    delete request;
+}
+
+void Google_Cloud_Vision_https_Request(NetworkInterface *network){
+    printf("\n\nSending Google Cloud Vision a https request!\n\n");
+    
+    HttpsRequest* request = new HttpsRequest(network, SSL_CA_PEM, HTTP_POST, MBED_CONF_APP_GC_VISION_API_URL);
+    request->set_header("Content-Type", "application/json");
+    
+    const char body[] = "{\"requests\": [{\"image\": {\"source\": {\"imageUri\": \"https://cloud.google.com/vision/images/rushmore.jpg\"}}, \"features\": [{\"type\": \"LABEL_DETECTION\", \"maxResults\": 5}]}]}";
+    HttpResponse* response = request->send(body, strlen(body));
     
     //Print Http Response
     printf("Status: %d - %s\n", response->get_status_code(), response->get_status_message().c_str());
@@ -85,7 +126,7 @@ int main() {
     wifi = WiFiInterface::get_default_instance();
     if (!wifi) {
         printf("ERROR: No WiFiInterface found.\n");
-        //return -1;
+        return -1;
     }
     
     //Connect to wifi access point specified in mbed_app.json
@@ -98,9 +139,13 @@ int main() {
     //Send a http request to the Twilio REST API
     Twilio_https_Request(wifi);
     
+    //Send a http request to the Twilio REST API
+    Google_Cloud_Vision_https_Request(wifi);
+    
     //Disconnect from wifi access point
     wifi->disconnect();
     printf("\nDone: Wifi has successfully disconnected.\n");
+    
     
     Blink_LED();
 }
